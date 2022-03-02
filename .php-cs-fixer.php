62b18f858a619250a3e6af5a696150e99c89b156
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2022-03-02 07:26:11 -0600

    add more rules to php cs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -13,6 +13,22 @@ return (new Config)
     ->setFinder($finder)
     ->setRiskyAllowed(true)
     ->setRules([
-        "@PhpCsFixer" => true,
-        "@PhpCsFixer:risky" => true,
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
+        'phpdoc_summary' => false,
+        'header_comment' => [
+            'header' => <<<EOT
+      _____       _           _
+     / ____|     | |         | |
+    | (___  _ __ | | ___   __| | ___
+     \___ \| '_ \| |/ _ \ / _` |/ _ \
+     ____) | |_) | | (_) | (_| |  __/
+    |_____/| .__/|_|\___/ \__,_|\___|
+           | |
+           |_|
+
+    Your next UX virion. <3 DiamondStrider1
+    EOT,
+            'location' => 'after_open',
+        ]
     ]);
